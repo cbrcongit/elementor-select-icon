089e08d36499bebcbac6c2a6d2aa66aa4195ebb2
Unknown <canberraconversion@gmail.com>
2018-02-01 15:51:43 +1100

    missed the main functino file somewhow... added now

--- elementor-iconbox/elementor-iconbox.php
+++ elementor-iconbox/elementor-iconbox.php
@@ -1,6 +1,14 @@
 <?php
-// if ( ! defined( 'ABSPATH' ) ) exit;
-add_action( 'elementor/init', function() { 
+/**
+ * Plugin Name: Custom Elementor IconBox
+ * Description: Override Elementor's 'Icon' control to include custom icon packs.
+ * Author: @cbrcongit, @albionselimaj, @ryanlabelle
+ * Version: 0.4
+ * Site: https://github.com/pojome/elementor/issues/110
+ */
+add_action( 'elementor/init', function() {
+if ( ! defined( 'ABSPATH' ) ) exit;
+
 class Elementor_Control_Icon extends Elementor\Base_Control {
 	
 	public function get_type() {
@@ -8,22 +16,18 @@ class Elementor_Control_Icon extends Elementor\Base_Control {
 	}
 
 	public static function get_icons() {
-		$icons = [];
-		// Get arrays of icons.
-		// $font_awesome_icons = require  PLUG_PATH . 'icons/font-awesome.php';
-		// $material_icons = require PLUG_PATH . 'icons/material-icons.php';
-		$custom_icons = require PLUG_PATH . 'icons/icomoon.php';
-			// foreach ($font_awesome_icons as $icon) {
-			// 	$icons["fa {$icon}"] = str_replace('fa-', '', $icon);
-			// }
-			// foreach ($material_icons as $icon) {
-			// 	$icons["material-icons {$icon}"] = $icon;
-			// }
-			foreach ($custom_icons as $icon) {
-				$icons[$icon] = str_replace('icon-', '', $icon);
-			}
-			return $icons;
-	}
+		return [
+			'icon-noun_977768' => 'pro',
+	        'icon-noun_977793' => 'safety',
+	        'icon-noun_1245026' => 'bicycle',
+	        'icon-noun_1248182' => 'engineer',
+	        'icon-noun_1258170' => 'engineer solve',
+	        'icon-noun_1267333' => 'humans',
+	        'icon-noun_1273446' => 'engineer fit',
+	        'icon-noun_1273443' => 'manager',
+	        'icon-noun_1278287' => 'weather cold',
+	        'icon-noun_977698' => 'handy',
+    	];
 
 	protected function get_default_settings() {
 		return [
@@ -49,9 +53,8 @@ class Elementor_Control_Icon extends Elementor\Base_Control {
 		<# } #>
 		<?php
 	}
-}
+};
+wp_enqueue_style('icomoon', plugin_dir_url( __FILE__ ) . 'style.css' );
 });
 
-add_action('elementor/controls/controls_registered', function($el) {
-	$el->register_control('icon', new Elementor_Control_Icon);
-});
+?>
